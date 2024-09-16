2a8a4d52680242b71009795b69a23dbcea9ae3ae
Alex Fânaț-Dănuț <oaVa-o@outlook.com>
2024-09-16 07:07:42 -0700

    Fix build and turn all build features back on.
    Make `gtk-mac-integration` and `gobject-introspection` run-time dependencies, with `zlib` and `glib`, and add `cmake` and `pango` as build-time dependencies.

--- gtkwave.rb
+++ gtkwave.rb
@@ -3,23 +3,27 @@ class Gtkwave < Formula
   homepage "https://gtkwave.github.io/gtkwave/"
   license "GPL-2.0-or-later"
   head "https://github.com/gtkwave/gtkwave.git", branch: "master"
-  revision 4
+  revision 5
 
-  depends_on "desktop-file-utils" => :build # for update-desktop-database
+  depends_on "desktop-file-utils" => :build
   depends_on "shared-mime-info" => :build
-  depends_on "gobject-introspection" => :build
-  depends_on "gtk-mac-integration" => :build
+  depends_on "gtk-mac-integration"
+  depends_on "gobject-introspection"
+  depends_on "cmake" => :build
+  depends_on "pango" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "glib"
   depends_on "gtk+3"
   depends_on "gtk4"
+  depends_on "zlib"
 
   def install
     ENV["DESTDIR"] = "/"
-    system "meson", "setup", "build", "-Dintrospection=false", *std_meson_args
+
+    system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
-
 end
